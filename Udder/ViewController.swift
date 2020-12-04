e7e1da90a3200c4c8c9a414a2ae8bbf3721fe541
DylanK616 <dylank@codecabin.co.za>
2020-12-04 08:56:44 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Dylan James Kotze on 2020/12/04.
+//  Copyright © 2020 CodeCabin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
